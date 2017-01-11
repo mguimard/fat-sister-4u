13fc517ef03b9e379a3113c1407c44b157d03103
morgan <morgan.guimard@docdoku.com>
2017-01-11 15:36:00 +0100

    Add TTS helper

---
+++ AndroidApp/HackAThon/mobile/src/main/java/com/docdoku/hackaton/hackathon/TTS.java
@@ -0,0 +1,34 @@
+package com.docdoku.hackaton.hackathon;
+
+import android.content.Context;
+import android.speech.tts.TextToSpeech;
+
+import java.util.Locale;
+
+/**
+ * TTS helper
+ * <p/>
+ * Usage (as easy as abc):
+ * <p/>
+ * TTS tts = new TTS(getApplicationContext());
+ * tts.say("Salut mes ptites beautés");
+ */
+public class TTS {
+
+    private TextToSpeech t;
+
+    public TTS(Context context) {
+        t = new TextToSpeech(context, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status != TextToSpeech.ERROR) {
+                    t.setLanguage(Locale.FRENCH);
+                }
+            }
+        });
+    }
+
+    public void say(String s) {
+        t.speak(s, TextToSpeech.QUEUE_FLUSH, null, null);
+    }
+}
